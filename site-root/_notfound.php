b4cbdaa4d945946c583623b98ed30f31af1a94c2
Chris Alfano <chris@jarv.us>
2018-10-12 19:28:11 +0000

    feat: render index.html for sub-directory routes

--- site-root/_notfound.php
+++ site-root/_notfound.php
@@ -2,16 +2,38 @@
 
 $path = Site::$pathStack;
 $filename = array_pop($path);
+$filenameIsExtensionless = $filename && strpos($filename, '.') === false;
 
 if (
-    $filename
-    && strpos($filename, '.') === false
-    && $htmlNode = Site::resolvePath(array_merge(
-        [ 'site-root' ],
-        $path,
-        [ $filename . '.html' ]
-    ))
+    (
+        $filenameIsExtensionless
+        && $htmlNode = Site::resolvePath(array_merge(
+            [ 'site-root' ],
+            $path,
+            [ $filename . '.html' ]
+        ))
+    )
+    || (
+        ($filenameIsExtensionless || !$filename)
+        && (
+            $treeNode = Site::resolvePath(array_filter(array_merge(
+                [ 'site-root' ],
+                $path,
+                [ $filename ]
+            )))
+        )
+        && $treeNode instanceof SiteCollection
+        && (
+            $htmlNode = $treeNode->getChild('index.html')
+        )
+    )
 ) {
+    // ensure trailing slash
+    if ($treeNode && $filename) {
+        Site::redirect(array_merge(Site::$pathStack, ['']));
+    }
+
+    // render HTML as response
     $htmlNode->outputAsResponse();
 } else {
     return RequestHandler::throwNotFoundError();
